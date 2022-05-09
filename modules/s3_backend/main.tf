e601e9663911fb5216690b200269ccdef4354928
Carlos Gonzalez <carlos.gonzalez@with-madrid.com>
2022-05-09 16:23:03 +0200

    removed variables files from module

--- modules/s3_backend/main.tf
+++ modules/s3_backend/main.tf
@@ -1,7 +1,5 @@
 
-# ------------------------------------------------------------------------------
-# CREATE THE S3 BUCKET
-# ------------------------------------------------------------------------------
+# Create the S3 bucket
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "${var.organization}-${var.bucket_prefix}-terraform-state"
   lifecycle {
